eef8890c36546ecf5533098db3917b74efad4c28
jaiom <jaiomrola@gmail.com>
2023-05-14 16:38:01 -0500

    added email marketing

--- docs/script.js
+++ docs/script.js
@@ -14,3 +14,41 @@ window.addEventListener('resize', () => {
         document.body.classList.remove('resize-animation-stopper');
     }, 400);
 });
+
+
+
+function SendMail() {
+    var params = {
+        from_name: document.getElementById("from_name").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById("message").value,
+        phone: document.getElementById("phone").value,
+        web_url: document.getElementById("web_url").value,
+
+        web_creation: document.getElementById("web_creation").checked,
+        seo: document.getElementById("seo").checked,
+        marketing: document.getElementById("marketing").checked,
+    }
+    emailjs.send("service_qrj6ggp", "template_2bbd5jv", params).then(function (res) {
+       openPopup()
+    })
+}
+
+let popup = document.getElementById("popup")
+
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
+
+$('#form').on('submit',function (e) {
+    e.preventDefault();
+    SendMail()
+    console.log('done')
+ 
+ }
+
+)
