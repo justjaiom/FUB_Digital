a7884f7c1e3c5782c6145b3ef1b9d580a4f5cfbd
justjaiom <jaiomrola@gmail.com>
2023-06-04 10:54:32 -0500

    remove animations from mobile

--- docs/animate.js
+++ docs/animate.js
@@ -6,39 +6,43 @@ const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     // Check if the element is in the viewport
     if (entry.isIntersecting) {
-      // Check if the element has any of the classes that need animation
+      // Check if the screen size is greater than 940
+      const screenSize = window.innerWidth;
+      if (screenSize > 940) {
+        // Check if the element has any of the classes that need animation
 
-      // Add the animation class to the current element based on its class
-      if (entry.target.classList.contains('fade-in-left')) {
-        entry.target.classList.add('animate__animated', 'animate__fadeInLeft');
-      }
-      if (entry.target.classList.contains('fade-in-right')) {
-        entry.target.classList.add('animate__animated', 'animate__fadeInRight');
-      }
-      if (entry.target.classList.contains('fade-in-up')) {
-        entry.target.classList.add('animate__animated', 'animate__fadeInUp');
-      }
-      if (entry.target.classList.contains('fade-in')) {
-        entry.target.classList.add('animate__animated', 'animate__fadeIn');
-      }
-      if (entry.target.classList.contains('fadeintop')) {
-        entry.target.classList.add('animate__animated', 'animate__fadeInDown');
-      }
-      if (entry.target.classList.contains('fadeinbottomleft')) {
-        entry.target.classList.add('animate__animated', 'animate__fadeInBottomLeft');
-      }
-      if (entry.target.classList.contains('fadeintopright')) {
-        entry.target.classList.add('animate__animated', 'animate__fadeInTopRight');
-      }
-      if (entry.target.classList.contains('fadeinbottom')) {
-        entry.target.classList.add('animate__animated', 'animate__fadeInUp');
-      }
-      if (entry.target.classList.contains('fadeinbottomright')) {
-        entry.target.classList.add('animate__animated', 'animate__fadeInBottomRight');
-      }
+        // Add the animation class to the current element based on its class
+        if (entry.target.classList.contains('fade-in-left')) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInLeft');
+        }
+        if (entry.target.classList.contains('fade-in-right')) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInRight');
+        }
+        if (entry.target.classList.contains('fade-in-up')) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInUp');
+        }
+        if (entry.target.classList.contains('fade-in')) {
+          entry.target.classList.add('animate__animated', 'animate__fadeIn');
+        }
+        if (entry.target.classList.contains('fadeintop')) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInDown');
+        }
+        if (entry.target.classList.contains('fadeinbottomleft')) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInBottomLeft');
+        }
+        if (entry.target.classList.contains('fadeintopright')) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInTopRight');
+        }
+        if (entry.target.classList.contains('fadeinbottom')) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInUp');
+        }
+        if (entry.target.classList.contains('fadeinbottomright')) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInBottomRight');
+        }
 
-      // Stop observing the element to avoid triggering the animation multiple times
-      observer.unobserve(entry.target);
+        // Stop observing the element to avoid triggering the animation multiple times
+        observer.unobserve(entry.target);
+      }
     }
   });
 });
